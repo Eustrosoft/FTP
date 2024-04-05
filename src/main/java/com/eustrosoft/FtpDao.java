1ad7ba3aab8a6bb7bb78eafdfb05b83213f097c2
Yadzuka <yadzuka@eustrosoft.org>
2024-04-06 01:58:14 +0300

    Updated version qdbpool version, but same version number still. Updated servet, added processor class and 2 processing consumers to process different path variations

--- src/main/java/com/eustrosoft/FtpDao.java
+++ src/main/java/com/eustrosoft/FtpDao.java
@@ -3,7 +3,6 @@ package com.eustrosoft;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.SequenceInputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
